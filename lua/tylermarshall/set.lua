144463ced371906963fe23213cf2091f2fce90c2
Tyler Marshall <tyler@dovetaildigital.ca>
2023-06-28 20:53:47 -0700

    Updates files

--- lua/tylermarshall/set.lua
+++ lua/tylermarshall/set.lua
@@ -29,3 +29,7 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
+
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', {silent = true, expr= true})
+vim.api.nvim_set_keymap("i", "<C-H>", 'copilot#Previous()', {silent = true, expr= true})
+vim.api.nvim_set_keymap("i", "<C-K>", 'copilot#Next()', {silent = true, expr= true})
